2f7cb38297b436b0b798291f6478645f3babcc21
Ruben Soerdien <ruben.soerdien@hotmail.com>
2019-11-28 17:16:52 +0100

    test

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'OST Samenvatting',
+    description: 'Samenvatting van het vak OST',
+    // theme: 'simple',
+    base: '/ost-samenvatting/',
+    repo: 'rubennaatje/ost-samenvatting',
+    docsDir: 'docs',
+    themeConfig: {
+      author: 'Ruben Soerdien', // will display on the page footer
+    //   navbar: { // will display below the title
+    //     YOUR_SITE_NAME: 'YOUR_SITE_URL',
+    //   }
+    }
+  }
